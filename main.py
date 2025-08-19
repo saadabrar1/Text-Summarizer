2e82c41cfcb7bac537bcdbb4518bc79e518ec013
Saad Abrar <saadabrar1>
2025-08-19 11:35:02 +0500

    data validation nnotebook added

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from textSummarizer.pipeline.stage_02_data_validation import DataValidationTrainingPipeline
 from textSummarizer.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
 from textSummarizer.logging import logger
 
@@ -7,6 +8,18 @@ try:
     logger.info(f">>>> stage { STAGE_NAME} started <<<<")
     data_ingestion = DataIngestionTrainingPipeline()
     data_ingestion.main()
+    logger.info(f">>>> stage  { STAGE_NAME} completed <<<<")
+except Exception as e:
+    logger.exception(e)
+    raise e
+
+
+STAGE_NAME = "Data Validation Stage"
+
+try:
+    logger.info(f">>>> stage { STAGE_NAME} started <<<<")
+    data_validation = DataValidationTrainingPipeline()
+    data_validation.main()
     logger.info(f">>>> stage  { STAGE_NAME} completed <<<<")
 except Exception as e:
     logger.exception(e)
--- src/textSummarizer/config/configuration.py
+++ src/textSummarizer/config/configuration.py
@@ -3,7 +3,8 @@ sys.path.append("d:\\Text-Summarizer\\src")
 
 from textSummarizer.constants import *
 from textSummarizer.utils.common import read_yaml, create_directories
-from textSummarizer.entity import DataIngestionConfig
+from textSummarizer.entity import DataIngestionConfig , DataValidationConfig
+
 
 class ConfigurationManager:
     def __init__(
@@ -29,6 +30,18 @@ class ConfigurationManager:
              unzip_dir=config.unzip_dir
         )
         return Data_ingestion_config
+    def get_data_validation_config(self)->DataValidationConfig:
+        config = self.config.data_validation
+
+        create_directories([config.root_dir])
+
+        Data_validation_config = DataValidationConfig(
+             root_dir=config.root_dir,
+             STATUS_FILE=config.STATUS_FILE,
+             ALL_REQUIRED_FILES=config.ALL_REQUIRED_FILES,
+
+        )
+        return Data_validation_config
 
         
 
